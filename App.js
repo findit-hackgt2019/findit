a97ae82ebb187010f0b8dc820ff8c4d0c0df7482
Chris <chrisjobando@gmail.com>
2019-10-26 09:22:00 -0400

    Experimenting with markers

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import MapView from 'react-native-maps';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import RetroMapStyles from './assets/MapStyles/RetroMapStyles.json';
 import * as Permissions from 'expo-permissions';
+import Geocoder from 'react-native-geocoding';
 
 export default class App extends React.Component {
   state = {
     latitude: null,
-    longitude: null
+    longitude: null,
+    marker: null
+  }
+
+  renderMarkers() {
+    return this.props.places.map((place, i) => (
+      <Marker
+        key={i}
+        title={place.name}
+        coordinate={place.coods}
+      />
+    ));
   }
 
   async componentDidMount() {
@@ -17,19 +30,48 @@ export default class App extends React.Component {
       const response = await Permissions.askAsync(Permissions.LOCATION)
     }
 
+    Geocoder.init("AIzaSyCAjd5RY-n2Tm0Qm4wcDFYUuEOvJkX3bqI");
+
     navigator.geolocation.getCurrentPosition(
-      ({ coords: { latitude, longitude } }) => this.setState({ latitude, longitude }, () => console.log('State: ', this.state)),
-      (error) => console.log('Error', error)
-    )
+      ({ coords: { latitude, longitude } }) => {
+        this.setState({ latitude: latitude, longitude: longitude }, () => console.log('State: ', this.state)),
+          (err) => console.warn(err);
+        //this.geocodeLocation(latitude, longitude);
+        this.geocodeLocation2("375 18th St NW").then(res =>
+          this.setState({marker: res}, () => console.log('Marker Set')),
+            (err) => console.warn(err));
+      })
   }
+
+  async geocodeLocation(lat, lng) {
+    Geocoder.from({
+      latitude: lat,
+      longitude: lng
+    }).then(json => {
+      let addressComponent = json.results[0].address_components;
+      console.log(addressComponent);
+    }).catch(err => console.warn(err));
+  }
+
+  async geocodeLocation2(name) {
+    Geocoder.from(name)
+      .then(json => {
+        let addressComponent = json.results[0].geometry.location;
+        console.log('Coords: ', addressComponent);
+        return addressComponent;
+      }).catch(err => console.warn(err));
+  }
+
   render() {
-    const { latitude, longitude } = this.state
+    const { latitude, longitude, marker } = this.state
 
     if (latitude) {
       return (
         <MapView
+        provider = { PROVIDER_GOOGLE }
           showsUserLocation
-          style = {{ flex: 1 }}
+          style={{ flex: 1 }}
+          customMapStyle={ RetroMapStyles }
           initialRegion = {{
             latitude,
             longitude,
@@ -37,13 +79,14 @@ export default class App extends React.Component {
             longitudeDelta: 0.0421
           }}
           >
+            {marker != null && <Marker coordinate={ marker.lat, marker.lng } />}
         </MapView>
       )
     }
 
     return (
       <View style= {{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>We need your permission!</Text>
+        <Text>Change your location permissions!</Text>
       </View>
     )
   }
